a315d786ebbd052c1c03238de0bf56c864813d73
Sem Schreuder <semschreuder@live.nl>
2019-06-16 23:28:14 +0200

    draws rotations with path

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -9,8 +9,8 @@
 #include "MainComponent.h"
 #include "../../HybridApi/Utilities/Utilities.h"
 
-int screenSizeX = 1920/2;
-int screenSizeY = 1080/2;
+int screenSizeX = 1920;
+int screenSizeY = 1080;
 
 //==============================================================================
 MainComponent::MainComponent()
@@ -28,25 +28,28 @@ MainComponent::~MainComponent()
 //==============================================================================
 void MainComponent::paint (Graphics& g)
 {
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
-    
     
     g.fillAll (Colours::darkgrey);
-    g.setColour (Colours::darkblue);
 
-    
-    g.setColour (Colours::sandybrown);
-    
     for (int i=0;i<rect.sIndex;i++)
     {
    
-        float x =         rect.xArrayPtr[(4*i)]*screenSizeX;
-        float y =         rect.yArrayPtr[(4*i)]*screenSizeY;
-        float width =     (rect.xArrayPtr[(4*i+1)] - rect.xArrayPtr[(4*i)])*screenSizeX;
-        float height =    (rect.yArrayPtr[(4*i+3)] - rect.yArrayPtr[(4*i+1)])*screenSizeY;
+        float x =         rect.xArrayPtr[(4*i)] * screenSizeX;
+        float y =         rect.yArrayPtr[(4*i)] * screenSizeY;
+        float width =     (rect.xArrayPtr[(4*i+1)] - rect.xArrayPtr[(4*i)]) * screenSizeX;
+        float height =    (rect.yArrayPtr[(4*i+3)] - rect.yArrayPtr[(4*i+1)]) * screenSizeY;
         
-        float sliceColor = (i*0.1)/(rect.sIndex*0.1);
-        cout << sliceColor << endl;
+        float sliceColor = (i*0.1)/(rect.sIndex*0.1) ;
+        
+        
+        /*
+        rect.xArrayPtr[(4*i)],  rect.yArrayPtr[(4*i)],      //top left
+        rect.xArrayPtr[(4*i+1)],rect.yArrayPtr[(4*i+1)],    //top right
+        rect.xArrayPtr[(4*i+2)],rect.yArrayPtr[(4*i+2)],    //bottom right
+        rect.xArrayPtr[(4*i+3)],rect.yArrayPtr[(4*i+3)],
+         
+         */
+        
 
         auto tileColor1  =  Colour::fromHSV (sliceColor,    // hue
                                               1.f,    // saturation
@@ -59,14 +62,35 @@ void MainComponent::paint (Graphics& g)
                                               0.7f,    // brightness
                                               0.6f);   // alpha,   // red
         
+        
+        
         Rectangle<float> slice (x,y,width,height);
-        g.fillCheckerBoard (slice, 10, 10, tileColor1, tileColor2);
+        
+        
+        
+        
+         Path path;
+         path.startNewSubPath (Point<float> (rect.xArrayPtr[(4*i)] * screenSizeX , rect.yArrayPtr[(4*i)] * screenSizeY));
+         path.lineTo (Point<float>          (rect.xArrayPtr[(4*i)+1] * screenSizeX , rect.yArrayPtr[(4*i)+1] * screenSizeY));
+         path.lineTo (Point<float>          (rect.xArrayPtr[(4*i)+2] * screenSizeX , rect.yArrayPtr[(4*i)+2] * screenSizeY));
+         path.lineTo (Point<float>          (rect.xArrayPtr[(4*i)+3] * screenSizeX , rect.yArrayPtr[(4*i)+3] * screenSizeY));
+         path.closeSubPath();
+        g.setColour(tileColor1);
+         g.fillPath (path);
+        
+         
+        
+        
+        
+        
+        
+        
         
         g.setFont (20.0f);
         g.setColour (Colours::white);
         g.drawText("smartAss", x, y, width, height,Justification::centred);
         g.setColour (Colours::black);
-        g.drawRect(x, y, width, height);
+        
         
         
         
