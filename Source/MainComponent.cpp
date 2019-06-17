c2cb52032554cba3a166a47c283b885d1ce920ed
Sem Schreuder <semschreuder@live.nl>
2019-06-17 16:17:16 +0200

    draws black center text sqaure

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -33,68 +33,46 @@ void MainComponent::paint (Graphics& g)
 
     for (int i=0;i<rect.sIndex;i++)
     {
-   
+        /*
         float x =         rect.xArrayPtr[(4*i)] * screenSizeX;
         float y =         rect.yArrayPtr[(4*i)] * screenSizeY;
         float width =     (rect.xArrayPtr[(4*i+1)] - rect.xArrayPtr[(4*i)]) * screenSizeX;
-        float height =    (rect.yArrayPtr[(4*i+3)] - rect.yArrayPtr[(4*i+1)]) * screenSizeY;
-        
-        float sliceColor = (i*0.1)/(rect.sIndex*0.1) ;
-        
-        
-        /*
-        rect.xArrayPtr[(4*i)],  rect.yArrayPtr[(4*i)],      //top left
-        rect.xArrayPtr[(4*i+1)],rect.yArrayPtr[(4*i+1)],    //top right
-        rect.xArrayPtr[(4*i+2)],rect.yArrayPtr[(4*i+2)],    //bottom right
-        rect.xArrayPtr[(4*i+3)],rect.yArrayPtr[(4*i+3)],
-         
+        float height =    (rect.xArrayPtr[(4*i+1)] - rect.xArrayPtr[(4*i)]) * screenSizeX;
          */
         
-
+        float sliceColor = (i*0.1)/(rect.sIndex*0.1) + 0.1 ;
         auto tileColor1  =  Colour::fromHSV (sliceColor,    // hue
                                               1.f,    // saturation
                                               1.f,    // brightness
-                                              0.6f);   // alpha,   // red
+                                              0.8f);   // alpha,   // red
         
-        
+        /*
         auto tileColor2  =  Colour::fromHSV (sliceColor,    // hue
                                               1.f,    // saturation
                                               0.7f,    // brightness
                                               0.6f);   // alpha,   // red
-        
-        
-        
-        Rectangle<float> slice (x,y,width,height);
-        
-        
-        
-        
-         Path path;
-         path.startNewSubPath (Point<float> (rect.xArrayPtr[(4*i)] * screenSizeX , rect.yArrayPtr[(4*i)] * screenSizeY));
-         path.lineTo (Point<float>          (rect.xArrayPtr[(4*i)+1] * screenSizeX , rect.yArrayPtr[(4*i)+1] * screenSizeY));
-         path.lineTo (Point<float>          (rect.xArrayPtr[(4*i)+2] * screenSizeX , rect.yArrayPtr[(4*i)+2] * screenSizeY));
-         path.lineTo (Point<float>          (rect.xArrayPtr[(4*i)+3] * screenSizeX , rect.yArrayPtr[(4*i)+3] * screenSizeY));
-         path.closeSubPath();
+        */
+
+        Path path;
+        path.startNewSubPath (Point<float> (rect.xArrayPtr[(4*i)]   * screenSizeX , rect.yArrayPtr[(4*i)]   * screenSizeY));
+        path.lineTo (Point<float>          (rect.xArrayPtr[(4*i)+1] * screenSizeX , rect.yArrayPtr[(4*i)+1] * screenSizeY));
+        path.lineTo (Point<float>          (rect.xArrayPtr[(4*i)+2] * screenSizeX , rect.yArrayPtr[(4*i)+2] * screenSizeY));
+        path.lineTo (Point<float>          (rect.xArrayPtr[(4*i)+3] * screenSizeX , rect.yArrayPtr[(4*i)+3] * screenSizeY));
+        path.closeSubPath();
         g.setColour(tileColor1);
-         g.fillPath (path);
+        g.fillPath (path);
         
-         
+        // rotation =  <InputRect orientation="0.25306850671768188477">
+        // Point<float> center = path.getBounds().getCentre();
         
-        
-        
-        
-        
-        
-        
+        g.setColour(Colour::fromHSV(1, 0, 0.2, 1));
+        Rectangle<float> slice (path.getBounds().getCentreX()-30,path.getBounds().getCentreY()-20,60,40);
+        g.fillRect(slice);
+
         g.setFont (20.0f);
         g.setColour (Colours::white);
-        g.drawText("smartAss", x, y, width, height,Justification::centred);
-        g.setColour (Colours::black);
-        
-        
-        
-        
-    
+        g.drawText("hey", path.getBounds(), Justification::centred);
+
     }
 
 }
