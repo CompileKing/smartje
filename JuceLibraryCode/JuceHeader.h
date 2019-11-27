750f23a94be20b109ee8edbc7d08959b4d4c2778
Sem Schreuder <semschreuder@live.nl>
2019-11-27 15:28:39 +0100

    name change yet again

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -47,8 +47,8 @@
 #if ! JUCE_DONT_DECLARE_PROJECTINFO
 namespace ProjectInfo
 {
-    const char* const  projectName    = "Smart Ass";
-    const char* const  companyName    = "";
+    const char* const  projectName    = "Smart A.S.S";
+    const char* const  companyName    = "PixelClone";
     const char* const  versionString  = "1.0.0";
     const int          versionNumber  = 0x10000;
 }
